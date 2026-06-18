d1575972d399f2c6e4d1cf708d3fc28d6ad3eb9c
Suhaila <188330098+Su-debug-glitch@users.noreply.github.com>
2026-06-18 10:49:35 +0200

    Creación y resolución de los ejercicios para practicar bucles for

---
+++ 03_loops/02_loop_for_exercises.py
@@ -0,0 +1,62 @@
+###
+# EJERCICIOS (for)
+###
+
+import os
+os.system("cls")
+
+# Ejercicio 1: Imprimir números pares
+# Imprime todos los números pares del 2 al 20 (inclusive) usando un bucle for.
+print("\nEjercicio 1:")
+pares = [num for num in range(2, 21, 2)]
+print(pares)
+
+# Ejercicio 2: Calcular la media de una lista
+# Dada la siguiente lista de números:
+# numeros = [10, 20, 30, 40, 50]
+# Calcula la media de los números usando un bucle for.
+print("\nEjercicio 2:")
+numeros = [10, 20, 30, 40, 50]
+suma = 0
+for numero in numeros:
+    suma += numero
+media = suma / len(numeros)
+print(f"La media es: {media}")
+
+# Ejercicio 3: Buscar el máximo de una lista
+# Dada la siguiente lista de números:
+# numeros = [15, 5, 25, 10, 20]
+# Encuentra el número máximo en la lista usando un bucle for.
+print("\nEjercicio 3:")
+numeros = [15, 5, 25, 10, 20]
+mayor = numeros[0]
+for numero in numeros:
+    if numero > mayor:
+        mayor = numero
+
+print(f"El número mayor es: {mayor}")
+
+# Ejercicio 4: Filtrar cadenas por longitud
+# Dada la siguiente lista de palabras:
+# palabras = ["casa", "arbol", "sol", "elefante", "luna"]
+# Crea una nueva lista que contenga solo las palabras con más de 5 letras
+# usando un bucle for y list comprehension.
+print("\nEjercicio 4:")
+palabras = ["casa", "arbol", "sol", "elefante", "luna"]
+nueva_lista = [word for word in palabras if len(word) > 5]
+print(nueva_lista)
+
+
+# Ejercicio 5: Contar palabras que empiezan con una letra
+# Dada la siguiente lista de palabras:
+# palabras = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
+# Pide al usuario que introduzca una letra.
+# Cuenta cuántas palabras en la lista empiezan con esa letra (sin diferenciar mayúsculas/minúsculas).
+print("\nEjercicio 5:")
+palabras = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
+letra = input("Introduce una letra: ").lower()
+contador = 0
+for palabra in palabras:
+    if palabra.lower().startswith(letra):
+        contador +=  1
+print(f"Hay {contador} palabra/s que empieza/n por {letra}")
